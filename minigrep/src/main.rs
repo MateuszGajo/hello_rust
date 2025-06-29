39ec7961094f08e750601fa0a4ad652eaac8addf
MateuszGajo <mateusz.gajo9@wp.pl>
2025-06-29 10:33:39 +0200

    minigrep program

---
+++ minigrep/src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::process;
+use minigrep::Config;
+
+fn main() {
+   let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprint!("Error: {}", err);
+            process::exit(1)
+    });
+
+    minigrep::run(config).unwrap_or_else(|err| {
+        eprint!("Error: {}", err);
+            process::exit(1)
+    })
+}
